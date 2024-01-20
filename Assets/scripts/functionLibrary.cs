e31bafc66415fe5f39c99d9eb5a22ec2de5305f1
amitshky <a7shky@gmail.com>
2024-01-20 16:55:05 +0545

    torus

--- Assets/scripts/functionLibrary.cs
+++ Assets/scripts/functionLibrary.cs
@@ -4,10 +4,10 @@ public static class FunctionLibrary
 {
 	public delegate Vector3 Function(float u, float v, float t);
 
-	private static Function[] _functions = { Wave, MultiWave, Ripple, Sphere };
+	private static Function[] _functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 	public enum FunctionSelecter : uint
 	{
-		Wave, MultiWave, Ripple, Sphere
+		Wave, MultiWave, Ripple, Sphere, Torus
 	}
 
 	public static Function GetFunction(FunctionSelecter index)
@@ -48,13 +48,28 @@ public static class FunctionLibrary
 
 	public static Vector3 Sphere(float u, float v, float t)
 	{
-		float r = Mathf.Cos(0.5f * Mathf.PI * v);
+		// parametric equation of a sphere
+		float radius = Mathf.Sin(0.5f * Mathf.PI * t);
 
 		Vector3 p;
-		p.x = r * Mathf.Sin(Mathf.PI * u);
-		p.y = Mathf.Sin(0.5f * Mathf.PI * v);
-		p.z = r * Mathf.Cos(Mathf.PI * u);
+		p.x = radius * Mathf.Cos(Mathf.PI * u) * Mathf.Sin(Mathf.PI * v);
+		p.y = radius * Mathf.Cos(Mathf.PI * v);
+		p.z = radius * Mathf.Sin(Mathf.PI * u) * Mathf.Sin(Mathf.PI * v);
 
+		return p;
+	}
+
+	public static Vector3 Torus(float u, float v, float t)
+	{
+		// parametric equation of a torus
+		float externalRadius = 1.0f;
+		float internalRadius = 0.25f;
+
+		Vector3 p;
+		p.x = (externalRadius + internalRadius * Mathf.Cos(Mathf.PI * u)) * Mathf.Cos(Mathf.PI * v);
+		p.y = internalRadius * Mathf.Sin(Mathf.PI * u);
+		p.z = (externalRadius + internalRadius * Mathf.Cos(Mathf.PI * u)) * Mathf.Sin(Mathf.PI * v);
+		
 		return p;
 	}
 }
